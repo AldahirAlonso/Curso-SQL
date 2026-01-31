bef6500de136a66cce8c9f9c93ff2cb756bc1a6b
Aldahir Alonso <aldahir0603@hotmail.com>
2026-01-31 16:05:49 -0600

    Modificador AND, OR y NOT.

---
+++ Statements/06_and_or_not.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE NOT email = "correo@gmail.com";
+
+SELECT * FROM users WHERE NOT email = "correo@gmail.com" AND age = 33;
+
+SELECT * FROM users WHERE NOT email = "correo@gmail.com" OR age = 33;
